5334f3199e236e07bee93a15d8a0bee88e41403d
aatif-21 <aatifsaman866@gmail.com>
2026-01-15 20:02:37 +0530

    Add files via upload

---
+++ question1_day43.c
@@ -0,0 +1,30 @@
+//Count spaces, digits, and special characters in a string.
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int spaces = 0, digits = 0, specialChars = 0;
+    int i;
+
+    printf("Enter a string:");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+        if (ch == ' ') {
+            spaces++;
+        } else if (ch >= '0' && ch <= '9') {
+            digits++;
+        } else if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+    
+        } else if (ch != '\n') { 
+            specialChars++;
+        }
+    }
+
+    printf("Number of spaces: %d\n", spaces);
+    printf("Number of digits: %d\n", digits);
+    printf("Number of special characters: %d\n", specialChars);
+
+    return 0;
+}
