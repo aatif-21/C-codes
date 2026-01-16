91d11d14c1df395f0f389a2524574d2a586abda9
aatif-21 <aatifsaman866@gmail.com>
2026-01-16 18:37:42 +0530

    Add files via upload

---
+++ question2_day44.c
@@ -0,0 +1,23 @@
+//Toggle case of each character in a string.
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - ('a' - 'A'); // Convert to uppercase
+        } else if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] + ('a' - 'A'); // Convert to lowercase
+        }
+    }
+
+    printf("Toggled case string: %s", str);
+
+    return 0;
+}
